f180026f8d7eeaa0357cc264342fd5cfb02df84c
Arto Bendiken <arto@dryproject.org>
2017-09-04 15:27:29 +0200

    Defined the test runner.

---
+++ test.cpp
@@ -0,0 +1,11 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include "drylib.hpp"
+
+#include <cstdlib> /* for EXIT_SUCCESS */
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[]) {
+  return EXIT_SUCCESS; // TODO
+}
