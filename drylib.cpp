aaa5c046c33afd0639ba0389530e9132b5f96e3e
Arto Bendiken <arto@dryproject.org>
2017-09-04 12:57:55 +0200

    Defined the core prelude for C++.

--- drylib.cpp
+++ drylib.cpp
@@ -8,13 +8,6 @@
 #error "DRYlib for C++ requires a C++14 compiler (CXXFLAGS='-std=c++14')"
 #endif
 
-#include "drylib.hpp"
-
 ////////////////////////////////////////////////////////////////////////////////
 
-// c++ -std=c++14 -DMAIN drylib.cpp
-#ifdef MAIN
-int main(int argc, char* argv[]) {
-  return 0; // TODO
-}
-#endif
+#include "drylib.hpp"
--- drylib.hpp
+++ drylib.hpp
@@ -4,10 +4,90 @@
 #error "DRYlib for C++ requires a C++ compiler"
 #endif
 
-#if __cplusplus < 201103L
-#error "DRYlib for C++ requires a C++11 compiler (CXXFLAGS='-std=c++11')"
+#if __cplusplus < 201402L
+#error "DRYlib for C++ requires a C++14 compiler (CXXFLAGS='-std=c++14')"
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////
 
-namespace dry {}
+#include <cstdint> /* for __WORDSIZE, std::*int_t */
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace dry {
+  using bool_ = bool;
+
+  using char_ = std::uint32_t;
+
+  struct complex;
+
+  using float_ = double;
+
+  using float32 = float;
+
+  using float64 = double;
+
+  using int_ = long;
+
+  using int8 = std::int8_t;
+
+  using int16 = std::int16_t;
+
+  using int32 = std::int32_t;
+
+  using int64 = std::int64_t;
+
+  using int128 = __int128; // TODO: protect with macro
+
+  struct integer;
+
+  using natural = integer;
+
+  struct rational;
+
+  struct real;
+
+#if __WORDSIZE == 32
+  using word = std::uint32_t;
+#elif __WORDSIZE == 64
+  using word = std::uint64_t;
+#else
+#error "unsupported size for __WORDSIZE"
+#endif
+
+  using word8 = std::uint8_t;
+
+  using word16 = std::uint16_t;
+
+  using word32 = std::uint32_t;
+
+  using word64 = std::uint64_t;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct dry::integer {
+  dry::int128 value; // TODO
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct dry::rational {
+  dry::integer numerator;
+  dry::integer denominator; /* denominator != 0 */
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct dry::real {
+  dry::float64 value; // TODO
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct dry::complex {
+  dry::real real;
+  dry::real imaginary;
+};
+
+////////////////////////////////////////////////////////////////////////////////
