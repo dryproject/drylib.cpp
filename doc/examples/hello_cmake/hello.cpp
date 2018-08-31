79f2e82a6228f9ce42bc7e70967761559ef14f21
Arto Bendiken <arto@dryproject.org>
2018-08-31 10:03:03 +0300

    Added a "Hello, world!" example for CMake.

---
+++ doc/examples/hello_cmake/hello.cpp
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <drylib.hpp>
+
+#include <cstdlib> /* for EXIT_SUCCESS */
+
+using namespace dry;
+
+int main(int argc, char* argv[]) {
+  // TODO: print "Hello, world!\n"
+  return EXIT_SUCCESS;
+}
