cb26be14b2ad2a835eea50d10cac40e716c49b9a
Arto Bendiken <arto@dryproject.org>
2018-09-01 07:22:15 +0300

    Added a "Hello, world!" example for GNU Autotools.

---
+++ doc/examples/hello_autoconf/hello.cpp
@@ -0,0 +1,16 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <drylib.hpp>
+
+#include <cstdlib> /* for EXIT_SUCCESS */
+
+using namespace dry;
+
+int main(int argc, char* argv[]) {
+  // TODO: print "Hello, world!\n"
+  return EXIT_SUCCESS;
+}
